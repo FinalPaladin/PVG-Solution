667eca44d48145fc2ee288600729f43d2005be06
Tran Canh Vinh <tcvinh.0702@gmail.com>
2025-11-14 16:01:55 +0700

    add base for vite config

--- PVGSolution/vite.config.ts
+++ PVGSolution/vite.config.ts
@@ -3,8 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/vay-thue-chap-30s/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
