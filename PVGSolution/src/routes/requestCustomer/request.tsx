ce0bfae57e3548883326068bd7f81a38745316d4
crvphungtm <phungpum96@gmail.com>
2025-12-01 16:16:37 +0700

    app en full name

--- PVGSolution/src/routes/requestCustomer/request.tsx
+++ PVGSolution/src/routes/requestCustomer/request.tsx
@@ -53,6 +53,7 @@ export default function RequestCustomerPage(): JSX.Element {
       let formData = new FormData();
       formData.append("Phone", form.phone);
       formData.append("ProductId", "3fa85f64-5717-4562-b3fc-2c963f66afa6");
+      formData.append("FullName", form.fullname);
       
       formData.append('dataJson', JSON.stringify([
           { key: "fullname", value: form.fullname, name: RequestCustomerLabels.fullname },
