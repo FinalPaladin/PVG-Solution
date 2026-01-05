ca68c1aafb839d02d550e6ea103e6e78f1e4747f
crvphungtm <phungpum96@gmail.com>
2026-01-05 11:16:50 +0700

    address

--- PVGSolution/src/App.tsx
+++ PVGSolution/src/App.tsx
@@ -15,6 +15,9 @@ function App() {
       </div>
       <footer className="w-full py-10 bg-white">
         <div className="max-w-[1400px] mx-auto flex items-center justify-center">
+          <p className="text-sm text-gray-600 text-center">163 phan đăng lưu, phường Cầu Kiệu, quận Phú Nhuận , TP.HCM</p>
+        </div>
+        <div className="max-w-[1400px] mx-auto flex items-center justify-center">
           <p className="text-sm text-gray-600 text-center">
             © 2025 Bản quyền thuộc về {webConfig.WebName}
           </p>
